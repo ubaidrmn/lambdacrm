6e32018cf26b02d047aea8228cc6ab8682bca622
ubaidrmn <rehmanubaid2003@gmail.com>
2025-07-21 22:16:26 +0500

    feat: Update CDK stack.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,18 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+
+import { join } from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaCrmCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const mainLambda = new lambda.Function(this, 'MainLambda', {
+      code: lambda.Code.fromAsset(join(__dirname, '../../backend/dist/src')),
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'index.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const restApi = new apigw.LambdaRestApi(this, 'MainRestApi', {
+      handler: mainLambda,
+      proxy: false,
+    });
 
+    const leads = restApi.root.addResource('leads');
 
+    leads.addMethod('GET');
+    leads.addMethod('POST');
+    leads.addMethod('DELETE');
+    leads.addMethod('PUT');
+
+    new cdk.CfnOutput(this, 'REST API URL', {
+      value: restApi.url ?? 'Something went wrong',
+    });
   }
 }
