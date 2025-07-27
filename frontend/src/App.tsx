681741eab9de71d842bc33d7785eee3a52eb126d
ubaidrmn <rehmanubaid2003@gmail.com>
2025-07-27 17:26:17 +0500

    refactor(frontend): Move `AuthContext.Provider` to a separate component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from "react-router"
 import DashboardPage from "@/features/dashboard/DashboardPage"
 import LeadsPage from "@/features/leads/LeadsPage"
 import ContactsPage from "@/features/contacts/ContactsPage"
@@ -7,36 +6,35 @@ import LoginPage from "@/features/auth/components/LoginPage"
 import AppLayout from "@/components/shared/AppLayout"
 import ThemeProvider from "@/components/shared/ThemeProvider"
 import LandingPage from "@/features/static/LandingPage"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { useState } from "react"
-import { type AuthContextValueType } from "./types/core"
-import AuthContext from "@/components/shared/AuthContext"
 import SignUpPage from "@/features/auth/components/SignUpPage"
-import { Toaster } from "sonner"
 import ConfirmUserPage from "@/features/auth/components/ConfirmUserPage"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Route, Routes } from "react-router"
+import { Toaster } from "sonner"
+import AuthContextProvider from "./components/shared/AuthContextProvider"
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [auth, setAuth] = useState<AuthContextValueType>({ isAuthenticated: false, user: null });
+
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="lambdacrm-theme">
-      <AuthContext.Provider value={{ auth: auth, setAuth: setAuth}}>
         <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<AppLayout><DashboardPage /></AppLayout>} />
-            <Route path="/leads" element={<AppLayout><LeadsPage /></AppLayout>} />
-            <Route path="/contacts" element={<AppLayout><ContactsPage /></AppLayout>} />
-            <Route path="/settings" element={<AppLayout><SettingsPage /></AppLayout>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/signup/confirm" element={<ConfirmUserPage />} />
-          </Routes>
+          <AuthContextProvider>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/dashboard" element={<AppLayout><DashboardPage /></AppLayout>} />
+              <Route path="/leads" element={<AppLayout><LeadsPage /></AppLayout>} />
+              <Route path="/contacts" element={<AppLayout><ContactsPage /></AppLayout>} />
+              <Route path="/settings" element={<AppLayout><SettingsPage /></AppLayout>} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/signup/confirm" element={<ConfirmUserPage />} />
+            </Routes>
+          </AuthContextProvider>
         </QueryClientProvider>
         <Toaster />
-      </AuthContext.Provider>
     </ThemeProvider>
   )
 }
