489130b3eb787282343f03f01bf8f3f16b72ae5a
ubaidrmn <rehmanubaid2003@gmail.com>
2025-08-07 03:33:11 +0500

    feat: Add sample chart to dashboard.

--- frontend/src/features/dashboard/DashboardPage.tsx
+++ frontend/src/features/dashboard/DashboardPage.tsx
@@ -1,13 +1,18 @@
+import { DashboardAreaChart } from "./DashboardAreaChart";
+import DashboardPageBreadcrumb from "./DashboardPageBreadcrumb";
+
 const DashboardPage = () => {
     return (
-        <div>
-            <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+        <>
+            <DashboardPageBreadcrumb />
+            <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight mt-2">
                 Dashboard
             </h2>
             <p className="leading-7 mt-2 mb-2">
                 Welcome to your dashboard. Here you can get an overview of your CRM activities, including leads, contacts, and settings.
             </p>
-        </div>
+            <DashboardAreaChart />
+        </>
     );
 }
 
---
+++ frontend/src/features/dashboard/DashboardPageBreadcrumb.tsx
@@ -0,0 +1,20 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Link } from "react-router";
+
+function DashboardPageBreadcrumb() {
+  return (
+    <Breadcrumb>
+        <BreadcrumbList>
+            <BreadcrumbItem>
+                <BreadcrumbLink asChild><Link to="/app/organizations">Organizations</Link></BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+                <BreadcrumbPage>Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+        </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
+
+export default DashboardPageBreadcrumb;
